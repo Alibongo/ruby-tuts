de7559c440d3cb7afdeca17cdce30680d7feb9b1
Alibongo <oioimolloy@gmail.com>
2017-03-20 18:41:17 +0000

    notes

--- wwc-HTTP-tut/file_reader.rb
+++ wwc-HTTP-tut/file_reader.rb
@@ -1,10 +1,17 @@
+# class is defined as FileReader
+# see where it ends.
 class FileReader
 
+#method takes parameter 'filename' which will be referenced
   def read_file(filename)
-    File.read(filename).each_line do |line|
+  #Below: this reads the file(referencing filename)
+    File.read(filename).each_line do |line| #for each line, we assign it to a var called line
+      # this begins a loop going over each line in the file.
+    # do |line| begins a Ruby Block that contains code to handle Line var.
     person = line.split(',')
     # age = person[1].strip
     city = person[2].strip
+    # puts is method for printing the particular line.
       puts city
     end
   end
@@ -18,7 +25,7 @@ class FileReader
     ages_file.close
   end
   def find_names_ages(filename)
-      
+
     output_file = File.open('names-ages.txt','w')
     File.read(filename).each_line do |line|
     person = line.split(',')
--- wwc-HTTP-tut/program.rb
+++ wwc-HTTP-tut/program.rb
@@ -1,6 +1,10 @@
-require_relative 'file_reader'
+require_relative 'file_reader' #this will ref FileReader - see blocked out code below
 require_relative 'http_reader'
+# An object is an instance of a class.
+# A class has the deinition. But you need to create an instance of it (ie object) to use it.
 
+
+# this is creating an object - file_reader
 # file_reader = FileReader.new
 # file_reader.read_file('addressbook.txt')
 # file_reader.find_ages('addressbook.txt')
